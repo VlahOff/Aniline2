ba6758485a7a8176d15c0caffb6ced3fcc820bd2
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-02 18:44:51 +0300

    On the coin details chart now shows which period is selected.

--- src/components/coinDetails/coinChart/CoinChart.jsx
+++ src/components/coinDetails/coinChart/CoinChart.jsx
@@ -5,6 +5,7 @@ import { getCoinDetails } from '../../../store/crypto-actions';
 import Button from '../../UI/button/Button';
 
 import classes from './CoinChart.module.css';
+import { useState } from 'react';
 
 const CoinChart = ({
   coinId,
@@ -13,6 +14,8 @@ const CoinChart = ({
   const dispatch = useDispatch();
   const coinDetailsOHLC = useSelector(state => state.crypto.coinDetailsOHLC);
   const theme = useSelector(state => state.ui.theme);
+
+  const [activePeriod, setActivePeriod] = useState(1);
 
   const series = [{
     data: coinDetailsOHLC
@@ -36,18 +39,37 @@ const CoinChart = ({
   const onPeriodSelection = (event) => {
     if (event.target.tagName === 'BUTTON') {
       dispatch(getCoinDetails(coinId, event.target.id));
+      setActivePeriod(event.target.id);
     }
   };
 
   return (
     <div className={`${classes['chart-container']} ${className}`}>
       <div className={classes['btn-wrapper']} onClick={onPeriodSelection}>
-        <Button className={classes.btn} id='1'>1D</Button>
-        <Button className={classes.btn} id='7'>1W</Button>
-        <Button className={classes.btn} id='14'>2W</Button>
-        <Button className={classes.btn} id='30'>1M</Button>
-        <Button className={classes.btn} id='60'>3M</Button>
-        <Button className={classes.btn} id='90'>6M</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '1' && classes['btn-active']}`} 
+        id='1'
+        >1D</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '7' && classes['btn-active']}`} 
+        id='7'
+        >1W</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '14' && classes['btn-active']}`} 
+        id='14'
+        >2W</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '30' && classes['btn-active']}`} 
+        id='30'
+        >1M</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '60' && classes['btn-active']}`} 
+        id='60'
+        >3M</Button>
+        <Button 
+        className={`${classes.btn} ${activePeriod == '90' && classes['btn-active']}`} 
+        id='90'
+        >6M</Button>
       </div>
       <ReactApexChart
         options={options}
