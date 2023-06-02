2712ed214635e31d681738e86e189b05c71caceb
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-02 21:33:23 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,88 +1,20 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 
+import ErrorNotification from './components/UI/errorNotification/ErrorNotification';
+import LoadingSpinner from './components/UI/loadingSpinner/LoadingSpinner';
+import Navigation from './components/navigation/Navigation';
 import { isUserLoggedIn } from './store/auth-actions';
 import { getTheme } from './store/ui-actions';
 
-import ProtectedRouteIsAuth from './ProtectedRouteIsAuth';
-import ProtectedRouteIsUser from './ProtectedRouteIsUser';
-import RootLayout from './RootLayout';
-import PageNotFound from './components/404page/PageNotFound';
-import Login from './components/auth/login/Login';
-import Register from './components/auth/register/Register';
-import CoinDetails from './components/coinDetails/CoinDetails';
-import NewCoins from './components/coinViews/newCoins/NewCoins';
-import TopHundred from './components/coinViews/topHundred/TopHundred';
-import CryptoConverter from './components/cryptoConverter/CryptoConverter';
-import Home from './components/home/Home';
-import Portfolio from './components/portfolio/Portfolio';
-import Profile from './components/profile/Profile';
+import classes from './App.module.css';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <RootLayout />,
-    errorElement: <PageNotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />
-      },
-      {
-        path: '/login',
-        element: (
-          <ProtectedRouteIsUser>
-            <Login />
-          </ProtectedRouteIsUser>
-        )
-      },
-      {
-        path: '/register',
-        element: (
-          <ProtectedRouteIsUser>
-            <Register />
-          </ProtectedRouteIsUser>
-        )
-      },
-      {
-        path: '/top-hundred',
-        element: <TopHundred />
-      },
-      {
-        path: '/new-coins',
-        element: <NewCoins />
-      },
-      {
-        path: '/coin-details/:id',
-        element: <CoinDetails />
-      },
-      {
-        path: '/crypto-converter',
-        element: <CryptoConverter />
-      },
-      {
-        path: '/portfolio',
-        element: (
-          <ProtectedRouteIsAuth>
-            <Portfolio />
-          </ProtectedRouteIsAuth>
-        )
-      },
-      {
-        path: '/profile',
-        element: (
-          <ProtectedRouteIsAuth>
-            <Profile />
-          </ProtectedRouteIsAuth>
-        )
-      }
-    ]
-  },
-]);
 
 function App() {
   const dispatch = useDispatch();
+  const isLoading = useSelector(state => state.ui.isLoading);
+  const theme = useSelector(state => state.ui.theme);
 
   useEffect(() => {
     dispatch(isUserLoggedIn());
@@ -90,7 +22,12 @@ function App() {
   }, []);
 
   return (
-    <RouterProvider router={router} />
+    <main data-theme={theme} className={classes.main}>
+      <ErrorNotification />
+      {isLoading && <LoadingSpinner />}
+      <Navigation />
+      <Outlet />
+    </main>
   );
 }
 
