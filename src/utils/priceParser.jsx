cc02ee2e3754467c4faf46ef26b4c33281153871
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-10 17:32:32 +0300

    .

--- src/utils/priceParser.jsx
+++ src/utils/priceParser.jsx
@@ -15,7 +15,6 @@ export const priceParser = (price = 0) => {
   const itStartsWithZero = price?.toString().startsWith('0.');
 
   const priceFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
     minimumFractionDigits: 2,
     maximumFractionDigits: itStartsWithZero ? 10 : undefined,
   });
