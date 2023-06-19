17c7f6ae002c12a4582f53eeda9e76e59d65912e
Dimitar Vlahov <dimitarvlahov24@gmail.com>
2023-06-19 13:14:28 +0300

    Added a confirmation dialog before deleting a transaction.

--- src/components/portfolio/editTransactionModal/EditTransactionModal.jsx
+++ src/components/portfolio/editTransactionModal/EditTransactionModal.jsx
@@ -1,14 +1,13 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { useForm } from '../../../hooks/useForm';
 import { portfolioActions } from '../../../store/portfolio';
-import {
-	deleteTransaction,
-	submitEditedTransaction,
-} from '../../../store/portfolio-actions';
+import { submitEditedTransaction } from '../../../store/portfolio-actions';
 import Button from '../../UI/button/Button';
 import Input from '../../UI/input/Input';
 import Modal from '../../UI/modal/Modal';
+import DeleteTransactionModal from '../deleteTransactionModal/DeleteTransactionModal';
 
 import classes from './EditTransactionModal.module.css';
 
@@ -21,6 +20,7 @@ const EditTransactionModal = () => {
 	const selectedTransaction = useSelector(
 		state => state.portfolio.selectedTransaction
 	);
+	const [startedDeletion, setStartedDeletion] = useState(false);
 
 	const { formValues, isFormValid, changeHandler } = useForm({
 		coinName: selectedTransaction?.name,
@@ -44,7 +44,7 @@ const EditTransactionModal = () => {
 	};
 
 	const onDeleteTransactionHandler = () => {
-		dispatch(deleteTransaction(selectedTransaction.transactionId));
+		setStartedDeletion(true);
 	};
 
 	return (
@@ -52,62 +52,69 @@ const EditTransactionModal = () => {
 			onClose={onCloseHandler}
 			className={classes.modal}
 		>
-			<form onSubmit={onSubmitHandler}>
-				<h1 className={classes.title}>Edit Transaction</h1>
-				<div className={classes['input-container']}>
-					<Input
-						label={'Coin'}
-						id={'coinId'}
-						value={formValues.coinName}
-						disabled={true}
-					/>
-					<Input
-						label={'Price per coin'}
-						id={'coinPrice'}
-						value={formValues.coinPrice}
-						error={formValues.coinPriceValid}
-						errorMessage={'Enter value greater than 0'}
-						type={'number'}
-						onChange={e => changeHandler(e, isNumberPositive)}
-					/>
-					<Input
-						label={'Quantity'}
-						id={'quantity'}
-						value={formValues.quantity}
-						error={formValues.quantityValid}
-						errorMessage={'Enter value greater than 0'}
-						type={'number'}
-						onChange={e => changeHandler(e, isNumberPositive)}
-					/>
-					<Input
-						label={'Total spent'}
-						id={'total'}
-						value={formValues.quantity * formValues.coinPrice}
-						disabled={true}
-					/>
-				</div>
-				<div className={classes['btn-container']}>
-					<Button
-						className={classes['cancel-btn']}
-						onClick={onCloseHandler}
-					>
-						Cancel
-					</Button>
-					<Button
-						className={classes['edit-btn']}
-						type={'submit'}
-						disabled={!isFormValid}
-					>
-						Edit
-					</Button>
-					<Button
-						className={classes['delete-btn']}
-						onClick={onDeleteTransactionHandler}
-					>
-						Delete
-					</Button>
-				</div>
-			</form>
+			{!startedDeletion ? (
+				<form onSubmit={onSubmitHandler}>
+					<h1 className={classes.title}>Edit Transaction</h1>
+					<div className={classes['input-container']}>
+						<Input
+							label={'Coin'}
+							id={'coinId'}
+							value={formValues.coinName}
+							disabled={true}
+						/>
+						<Input
+							label={'Price per coin'}
+							id={'coinPrice'}
+							value={formValues.coinPrice}
+							error={formValues.coinPriceValid}
+							errorMessage={'Enter value greater than 0'}
+							type={'number'}
+							onChange={e => changeHandler(e, isNumberPositive)}
+						/>
+						<Input
+							label={'Quantity'}
+							id={'quantity'}
+							value={formValues.quantity}
+							error={formValues.quantityValid}
+							errorMessage={'Enter value greater than 0'}
+							type={'number'}
+							onChange={e => changeHandler(e, isNumberPositive)}
+						/>
+						<Input
+							label={'Total spent'}
+							id={'total'}
+							value={formValues.quantity * formValues.coinPrice}
+							disabled={true}
+						/>
+					</div>
+					<div className={classes['btn-container']}>
+						<Button
+							className={classes['cancel-btn']}
+							onClick={onCloseHandler}
+						>
+							Cancel
+						</Button>
+						<Button
+							className={classes['edit-btn']}
+							type={'submit'}
+							disabled={!isFormValid}
+						>
+							Edit
+						</Button>
+						<Button
+							className={classes['delete-btn']}
+							onClick={onDeleteTransactionHandler}
+						>
+							Delete
+						</Button>
+					</div>
+				</form>
+			) : (
+				<DeleteTransactionModal
+					setStartedDeletion={setStartedDeletion}
+					transactionId={selectedTransaction.transactionId}
+				/>
+			)}
 		</Modal>
 	);
 };
